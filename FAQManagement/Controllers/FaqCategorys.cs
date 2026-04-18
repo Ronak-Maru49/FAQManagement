6e55d0e95a0ecc10ebb117ad3f22c063aa5f2cfd
MaruRonak <maruronak5@gmail.com>
2026-04-18 15:40:51 +0530

    category

---
+++ FAQManagement/Controllers/FaqCategorys.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FAQManagement.Controllers
+{
+    public class FaqCategorys : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
