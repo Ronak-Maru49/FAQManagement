5ccbecd131ab136fd776d7aaa866ad67b5ea0ba3
MaruRonak <maruronak5@gmail.com>
2026-04-20 17:14:10 +0530

    ai gen faq

---
+++ FAQManagement/Controllers/AiFaqGen.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FAQManagement.Controllers
+{
+    public class AiFaqGen : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
